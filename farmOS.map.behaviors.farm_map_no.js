0cc633a60f3c55861f14449e8689b1152ff72cb9
botlfarm <54162154+botlfarm@users.noreply.github.com>
2020-12-21 13:50:32 -0500

    BOTL Farm

--- farmOS.map.behaviors.farm_map_no.js
+++ farmOS.map.behaviors.farm_map_no.js
@@ -2,10 +2,10 @@
   farmOS.map.behaviors.farm_map_no = {
     attach: function (instance) {
 
-      // Add Norkart Flyfoto layer.
+      // Add layer.
       var opts = {
-        title: 'Norkart Flyfoto',
-        url: 'http://map04.eniro.no/geowebcache/service/tms1.0.0/aerial/{z}/{x}/{-y}.jpeg?c=2542211020&v=20160420',
+        title: 'Drone',
+        url: 'https://tiles.openaerialmap.org/5e068ec505e6590005c2f718/0/5e068ec505e6590005c2f719/{z}/{x}/{y}',
         group: 'Base layers',
       };
       instance.addLayer('xyz', opts);
